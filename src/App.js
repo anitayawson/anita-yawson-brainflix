a873dbcf744d9f70f5aebb94dea28e5ad4a491b0
Anita Yawson <anitayawson@gmail.com>
2024-01-16 21:43:29 -0500

    Desktop view styling for header component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <body className="App">
+    <div className="App">
       <Header />
-    </body>
+    </div>
   );
 }
 
