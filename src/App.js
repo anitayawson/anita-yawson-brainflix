03a8959a3fcb5a8a40dd367caeedba8a3cdd1846
Anita Yawson <anitayawson@gmail.com>
2024-01-17 22:09:27 -0500

    Add typography mixins, styles for video info component, adjust widths/heights for header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import VideoInfo from "./components/VideoInfo/VideoInfo";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <VideoPlayer />
+      <VideoInfo />
     </div>
   );
 }
