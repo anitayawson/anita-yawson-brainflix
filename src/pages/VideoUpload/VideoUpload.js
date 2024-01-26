bbfdf86bfc5640874c2102f98421511946f56850
Anita Yawson <anitayawson@gmail.com>
2024-01-26 00:09:31 -0500

    Add styling for elements

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -5,45 +5,47 @@ import publishIcon from "../../assets/images/icons/publish.svg";
 
 export default function VideoUpload() {
   return (
-    <main>
-      <h1 className="title">Upload Video</h1>
-      <div className="thumbnail__container">
-        <p className="thumbnail__label">Video Thumbnail</p>
-        <img
-          className="thumbnail__img"
-          src={videoThumbnail}
-          alt="video thumbnail"
-        />
-      </div>
-      <form className="form">
-        <label className="form__input-label" htmlFor="title">
-          Title Your Video
-        </label>
-        <input
-          className="form__input"
-          type="text"
-          id="title"
-          placeholder="Add a title to your video"
-        />
-        <label className="form__input-label" htmlFor="description">
-          Add A Video Description
-        </label>
-        <textarea
-          className="form__input"
-          id="description"
-          placeholder="Add a video description"
-          rows={4}
-        ></textarea>
-        <button className="form__publish-btn">
+    <main className="video-upload">
+      <div className="video-upload__wrapper">
+        <h1 className="title">Upload Video</h1>
+        <div className="thumbnail__container">
+          <p className="thumbnail__label">Video Thumbnail</p>
           <img
-            className="form__publish-icon"
-            src={publishIcon}
-            alt="add icon"
+            className="thumbnail__img"
+            src={videoThumbnail}
+            alt="video thumbnail"
           />
-          <span className="form__button-text">Publish</span>
-        </button>
-        <button className="form__cancel-btn">Cancel</button>
-      </form>
+        </div>
+        <form className="upload-form">
+          <label className="upload-form__input-label" htmlFor="title">
+            Title Your Video
+          </label>
+          <input
+            className="upload-form__input"
+            type="text"
+            id="title"
+            placeholder="Add a title to your video"
+          />
+          <label className="upload-form__input-label" htmlFor="description">
+            Add A Video Description
+          </label>
+          <textarea
+            className="upload-form__input"
+            id="description"
+            placeholder="Add a video description"
+            rows={4}
+          ></textarea>
+          <button className="upload-form__publish-btn">
+            <img
+              className="upload-form__publish-icon"
+              src={publishIcon}
+              alt="add icon"
+            />
+            <span className="upload-form__btn-text">Publish</span>
+          </button>
+          <button className="upload-form__cancel-btn">Cancel</button>
+        </form>
+      </div>
     </main>
   );
 }
