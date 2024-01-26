7a656f42f47dbdd01a84879c150d2cf91da05349
Anita Yawson <anitayawson@gmail.com>
2024-01-26 00:26:10 -0500

    Add path to upload button, fix styling and clear comments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,14 @@ import logo from "../../assets/images/logos/BrainFlix-logo.svg";
 import searchIcon from "../../assets/images/icons/search.svg";
 import uploadIcon from "../../assets/images/icons/upload.svg";
 import userImage from "../../assets/images/Mohan-muruge.jpg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo-container">
+      <Link to="/" className="header__logo-container">
         <img className="header__logo" src={logo} alt="BrainFlix Logo" />
-      </div>
+      </Link>
       <div className="header__search-user-container">
         <div className="header__search-container">
           <input
@@ -25,14 +26,14 @@ function Header() {
         </div>
         <img src={userImage} alt="user icon" className="header__user-img"></img>
       </div>
-      <button className="header__upload-btn">
+      <Link to="/upload" className="header__upload-btn">
         <img
           className="header__upload-icon"
           src={uploadIcon}
           alt="upload icon"
         />
         <span className="header__button-text">Upload</span>
-      </button>
+      </Link>
       <img
         src={userImage}
         alt="user icon"
