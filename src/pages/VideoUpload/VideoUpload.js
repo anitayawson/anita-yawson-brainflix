3d6eedfe36505762f6304b13b6a46d580c76d0ba
Anita Yawson <anitayawson@gmail.com>
2024-01-30 06:25:17 -0500

    Add styling for desktop breakpoint

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -8,45 +8,48 @@ export default function VideoUpload() {
     <main className="video-upload">
       <div className="video-upload__wrapper">
         <h1 className="title">Upload Video</h1>
-        <div className="thumbnail__container">
-          <p className="thumbnail__label">Video Thumbnail</p>
-          <img
-            className="thumbnail__img"
-            src={videoThumbnail}
-            alt="video thumbnail"
-          />
-        </div>
-        <form className="upload-form">
-          <label className="upload-form__input-label" htmlFor="title">
-            Title Your Video
-          </label>
-          <input
-            className="upload-form__input"
-            type="text"
-            id="title"
-            placeholder="Add a title to your video"
-          />
-          <label className="upload-form__input-label" htmlFor="description">
-            Add A Video Description
-          </label>
-          <textarea
-            className="upload-form__input"
-            id="description"
-            placeholder="Add a description to your video"
-            rows={4}
-          ></textarea>
-          <div className="upload-form__btn-container">
-            <button className="upload-form__publish-btn">
-              <img
-                className="upload-form__publish-icon"
-                src={publishIcon}
-                alt="add icon"
-              />
-              <span className="upload-form__btn-text">Publish</span>
-            </button>
-            <button className="upload-form__cancel-btn">Cancel</button>
+        <div className="upload-form__desktop">
+          <div className="thumbnail__container">
+            <p className="thumbnail__label">Video Thumbnail</p>
+            <img
+              className="thumbnail__img"
+              src={videoThumbnail}
+              alt="video thumbnail"
+            />
           </div>
-        </form>
+          <form className="upload-form">
+            <label className="upload-form__input-label" htmlFor="title">
+              Title Your Video
+            </label>
+            <input
+              className="upload-form__input"
+              type="text"
+              id="title"
+              placeholder="Add a title to your video"
+            />
+            <label className="upload-form__input-label" htmlFor="description">
+              Add A Video Description
+            </label>
+            <textarea
+              className="upload-form__input"
+              id="description"
+              placeholder="Add a description to your video"
+              rows={4}
+            ></textarea>
+          </form>
+        </div>
+
+        <div className="upload-form__btn-container">
+          <button className="upload-form__publish-btn">
+            <img
+              className="upload-form__publish-icon"
+              src={publishIcon}
+              alt="add icon"
+            />
+            <span className="upload-form__btn-text">Publish</span>
+          </button>
+          <button className="upload-form__cancel-btn">Cancel</button>
+        </div>
       </div>
     </main>
   );
