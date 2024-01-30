c63b7265afd8fda6cc99b984d9cd769f41c65da1
Anita Yawson <anitayawson@gmail.com>
2024-01-30 07:05:59 -0500

    Add notification & redirect route to home after form submisson

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -1,13 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import videoThumbnail from "../../assets/images/Upload-video-preview.jpg";
 import "./VideoUpload.scss";
 import publishIcon from "../../assets/images/icons/publish.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function VideoUpload() {
+  const navigate = useNavigate();
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setIsFormSubmitted(true);
+    setTitle("");
+    setDescription("");
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  };
+
   return (
     <main className="video-upload">
       <div className="video-upload__wrapper">
         <h1 className="title">Upload Video</h1>
+        {isFormSubmitted && (
+          <div className="upload-notification">
+            <h3>Your video has been uploaded successfully! Redirecting...</h3>
+          </div>
+        )}
         <div className="upload-form__desktop">
           <div className="thumbnail__container">
             <p className="thumbnail__label">Video Thumbnail</p>
@@ -25,7 +54,9 @@ export default function VideoUpload() {
               className="upload-form__input"
               type="text"
               id="title"
+              value={title}
               placeholder="Add a title to your video"
+              onChange={handleTitleChange}
             />
             <label className="upload-form__input-label" htmlFor="description">
               Add A Video Description
@@ -33,14 +64,20 @@ export default function VideoUpload() {
             <textarea
               className="upload-form__input"
               id="description"
+              value={description}
               placeholder="Add a description to your video"
               rows={4}
+              onChange={handleDescriptionChange}
             ></textarea>
           </form>
         </div>
 
         <div className="upload-form__btn-container">
-          <button className="upload-form__publish-btn">
+          <button
+            className="upload-form__publish-btn"
+            type="submit"
+            onClick={handleFormSubmit}
+          >
             <img
               className="upload-form__publish-icon"
               src={publishIcon}
