1f06dfecf7b39d4fe2f9535be73e9c20196ae111
Anita Yawson <anitayawson@gmail.com>
2024-01-29 22:46:44 -0500

    Update side-videos to use react router

--- src/components/NextVideos/NextVideos.js
+++ src/components/NextVideos/NextVideos.js
@@ -1,23 +1,13 @@
 import "./NextVideos.scss";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function NextVideos({ videos }) {
-  const navigate = useNavigate();
-
-  const handleSelectVideo = (clickedId) => {
-    navigate(`/videos/${clickedId}`);
-  };
-
   return (
     <section className="next-videos">
       <p className="next-videos__title">Next Videos</p>
       <div className="video-list">
         {videos.map((video) => (
-          <article
-            key={video.id}
-            className="video"
-            onClick={() => handleSelectVideo(video.id)}
-          >
+          <Link key={video.id} to={`/videos/${video.id}`} className="video">
             <div className="video__thumbnail-container">
               <img
                 className="video__thumbnail"
@@ -29,7 +19,7 @@ function NextVideos({ videos }) {
               <p className="video__title">{video.title}</p>
               <p className="video__creator">{video.channel}</p>
             </div>
-          </article>
+          </Link>
         ))}
       </div>
     </section>
