4d27fdb4befc8c68634a83ba5d0bc017bc698559
Anita Yawson <anitayawson@gmail.com>
2024-02-04 21:50:42 -0500

    fix form rendering for different conditions

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,43 +1,48 @@
+import React from "react";
 import "./CommentSection.scss";
 import userImage from "../../assets/images/Mohan-muruge.jpg";
 import addIcon from "../../assets/images/icons/add_comment.svg";
 import moment from "moment";
 
 function CommentSection({ selectedVideo }) {
-  if (!selectedVideo.comments) {
-    return null;
-  }
-
-  const comments = selectedVideo.comments;
+  const comments = selectedVideo.comments || [];
   const commentCount = comments.length;
+
+  const renderForm = commentCount === 0 || !selectedVideo.comments || comments;
 
   return (
     <section className="comments-section">
       <p className="comments-section__subheader">
-        {commentCount} {commentCount === 1 ? "Comment" : "Comments"}
+        {commentCount === 0
+          ? "No comments yet"
+          : `${commentCount} ${commentCount === 1 ? "Comment" : "Comments"}`}
       </p>
-      <form className="form">
-        <img className="form__user-img" src={userImage} alt="user icon" />
-        <div className="form__content-wrapper">
-          <label typeof="comment" className="form__input-label">
-            Join the conversation
-          </label>
-          <div className="form__tablet-flex-container">
-            <textarea
-              className="form__input"
-              id="comment"
-              placeholder="Add a new comment"
-              rows={4}
-            ></textarea>
-            <button className="form__add-comment-btn">
-              <img className="form__add-icon" src={addIcon} alt="add icon" />
-              <span className="form__button-text">Comment</span>
-            </button>
+
+      {renderForm && (
+        <form className="form">
+          <img className="form__user-img" src={userImage} alt="user icon" />
+          <div className="form__content-wrapper">
+            <label typeof="comment" className="form__input-label">
+              Join the conversation
+            </label>
+            <div className="form__tablet-flex-container">
+              <textarea
+                className="form__input"
+                id="comment"
+                placeholder="Add a new comment"
+                rows={4}
+              ></textarea>
+              <button className="form__add-comment-btn">
+                <img className="form__add-icon" src={addIcon} alt="add icon" />
+                <span className="form__button-text">Comment</span>
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      )}
+
       <div className="comments-list">
-        {selectedVideo.comments.map((comment) => (
+        {comments.map((comment) => (
           <article className="comment" key={comment.id}>
             <div className="comment__avatar-placeholder"></div>
             <div className="comment__body">
